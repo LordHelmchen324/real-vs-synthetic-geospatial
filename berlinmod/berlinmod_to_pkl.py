e256601f1c0caf52cbbf4e2ae3e71356693428a1
Jan Kaiser <jan.kaiser.email@googlemail.com>
2018-12-19 19:44:18 +0100

    Made sure all dataset to .pkl conversion ensure the 'standard' format

--- berlinmod/berlinmod_to_pkl.py
+++ berlinmod/berlinmod_to_pkl.py
@@ -9,16 +9,20 @@ print('Reading data from generated csv file ...')
 
 # Read data from generated file
 df = pd.read_csv('trips_%s.csv' % (name))
+
+# Rename, delete etc. columns in order to achieve the 'standard' format
+df['user'] = df['Moid']
 df['time'] = pd.to_datetime(df['Tstart'], format = '%Y-%m-%d %H:%M:%S')
 df['longitude'] = df['Xstart']
 df['latitude'] = df['Ystart']
 df['trip'] = df['Tripid']
-df.drop(['Tstart', 'Tend', 'Xstart', 'Xend', 'Ystart', 'Yend', 'Tripid'], axis=1, inplace=True)
+
+df.drop(['Moid', 'Tstart', 'Tend', 'Xstart', 'Xend', 'Ystart', 'Yend', 'Tripid'], axis=1, inplace=True)
 
 print('Changing the way the dataset is indexed ...')
 
 # Change the way the dataset is indexed
-df_reindexed = df.set_index(['Moid', 'time'])
+df_reindexed = df.set_index(['user', 'time'])
 df_reindexed.sort_index(inplace=True)
 
 print('Saving the dataset to .pkl ...')
